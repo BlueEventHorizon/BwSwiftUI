f603508bc28435a6a7b217f5a14ac9f71a9f9cef
k_terada <k_terada@yumemi.co.jp>
2022-08-26 21:22:44 +0900

    add extension

---
+++ Sources/BwSwiftUI/UIColor+Color.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Color.swift
+//  
+//
+//  Created by Katsuhiko Terada on 2022/08/14.
+//
+
+import SwiftUI
+
+public extension UIColor {
+    var color: Color {
+        Color(self)
+    }
+}
